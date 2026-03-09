ef994aab9c50def6fa7d96d490e49d3a71c438c8
Dmitriy Lyashenko <sv-dm@yandex.ru>
2026-03-09 16:44:35 +0300

    chore: remove default Storybook stories

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  devIndicators: false, // отключает черный кружок с буквой N
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default nextConfig
