df5110c8f56a9f8b05ae0f29ccb64232c97a1603
Uladzislau <u.shylko25@gmail.com>
2026-03-11 19:29:38 +0300

    feat: create reusable Button and Input components, add Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Form } from '@/src/widgets/form/ui/Form'
+import s from './page.module.css'
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <div className={s.container}>
+      <h1>Hello</h1>
+      <Form />
+    </div>
+  )
 }
---
+++ src/widgets/form/ui/Form.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { Input } from '@/src/shared/ui/Input/Input'
+import { EyeIcon } from '@/src/shared/ui/SVG/Icon'
+import { Button } from '@/src/shared/ui/Button/Button'
+import s from './Form.module.css'
+
+export const Form = () => {
+  return (
+    <div className={s.container}>
+      <h2>Sign Up</h2>
+      <Input label="Username" type="text" placeholder="Username" />
+      <Input label="Email" type="email" placeholder="Email" />
+      <Input label="Password" rightIcon={EyeIcon} type="password" rightIconClickable={true} placeholder="Password" />
+      <Input
+        label="Password confirmation"
+        type="password"
+        rightIcon={EyeIcon}
+        placeholder="Password confirmation"
+        rightIconClickable={true}
+      />
+      <Button variant={'primary'}>Sign Up</Button>
+      <p>Do you have an account?</p>
+      <Button variant={'ghost'}>Sign In</Button>
+    </div>
+  )
+}
