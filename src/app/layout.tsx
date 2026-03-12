27286608d183a1b328ad7171cb75394b1a6dac65
Dmitriy Lyashenko <sv-dm@yandex.ru>
2026-03-12 14:08:03 +0300

    feat: install TanStack Query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { QueryProvider } from './providers/query-provider'
+import { ReactNode } from 'react'
 
 const interSans = Inter({
   variable: '--font-inter',
@@ -16,11 +18,13 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${interSans.variable} antialiased`}>{children}</body>
+      <body className={`${interSans.variable} antialiased`}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   )
 }
