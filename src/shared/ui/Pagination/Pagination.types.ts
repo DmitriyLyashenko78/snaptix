dde4e57a0b4225e061904c4ab16e102b0ef98fb3
Oleg Kova <okovalchuk722@gmail.com>
2026-03-15 21:32:00 +0300

    feat (Pagination): add pagination - 100%

--- src/shared/ui/Pagination/Pagination.types.ts
+++ src/shared/ui/Pagination/Pagination.types.ts
@@ -2,4 +2,6 @@ export type PaginationProps = {
   currentPage: number
   setCurrentPage: (page: number) => void
   pagesCount: number
+  changePageSize: (pageSize: number) => void
+  pageSize: number
 }
--- src/shared/utils/getPaginationPages.ts
+++ src/shared/utils/getPaginationPages.ts
@@ -1,32 +1,21 @@
-const SIBLING_COUNT = 1
-
 export const getPaginationPages = (currentPage: number, pagesCount: number): (number | '...')[] => {
-  if (pagesCount <= 1) {
-    return []
-  }
-
-  const pages: (number | '...')[] = []
+  const SIBLING_COUNT = 1
 
   const leftSibling = Math.max(2, currentPage - SIBLING_COUNT)
   const rightSibling = Math.min(pagesCount - 1, currentPage + SIBLING_COUNT)
 
-  pages.push(1)
+  const showLeftDots = leftSibling > 3
+  const showRightDots = rightSibling < pagesCount - 2
 
-  if (leftSibling > 2) {
-    pages.push('...')
+  if (!showLeftDots && showRightDots) {
+    const leftRange = Array.from({ length: 5 }, (_, i) => i + 1)
+    return [...leftRange, '...', pagesCount]
   }
 
-  for (let page = leftSibling; page <= rightSibling; page++) {
-    pages.push(page)
+  if (showLeftDots && !showRightDots) {
+    const rightRange = Array.from({ length: 5 }, (_, i) => pagesCount - 4 + i)
+    return [1, '...', ...rightRange]
   }
 
-  if (rightSibling < pagesCount - 1) {
-    pages.push('...')
-  }
-
-  if (pagesCount > 1) {
-    pages.push(pagesCount)
-  }
-
-  return pages
+  return [1, '...', currentPage - 1, currentPage, currentPage + 1, '...', pagesCount]
 }
