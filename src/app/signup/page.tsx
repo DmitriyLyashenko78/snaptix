42b3d9ece3518c5667fc4aa35218b0ad7b43b676
Dmitriy Lyashenko <sv-dm@yandex.ru>
2026-03-16 11:32:27 +0300

    merge: resolve conflicts with Component1 branch

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,9 @@
-import { Form } from '@/src/widgets/form/ui/Form'
 import s from './signUp.module.css'
 
 export default function SignUpPage() {
   return (
     <div className={s.container}>
       <h1>SignUp Page</h1>
-      <Form />
     </div>
   )
 }
